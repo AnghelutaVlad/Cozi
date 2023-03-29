89a6752be47b3d2a0f7d0682c3fa05a33b4786e9
AnghelutaVlad <128615735+AnghelutaVlad@users.noreply.github.com>
2023-03-29 20:20:57 +0300

    Update Main.c

--- Main.c
+++ Main.c
@@ -1,9 +1,11 @@
 #include"Cozi.h"
 int main()
 {
-    int k;
+    int k,a;
     scanf("%d",&k);
     CoadaVector*q;
     q=umplerevector(k);
+    scanf("%d",&a);
+    pushVector(q,a);
     afisareVector(q);
 }
